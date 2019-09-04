7439e4f06be2f8c331e525485cd0e73483ad8083
Manassarn Manoonchai <manassarn@gmail.com>
2019-09-04 12:51:11 +0700

    :bug: Use wildcard match

--- 10fastfingers-helper.user.js
+++ 10fastfingers-helper.user.js
@@ -4,7 +4,7 @@
 // @author narze
 // @version 0.0.1
 // @description Quick restart
-// @match https://10fastfingers.com/
+// @match https://10fastfingers.com/*
 // @icon https://10fastfingers.com/favicon.ico
 // @downloadURL https://github.com/narze/userscripts/raw/master/10fastfingers-helper.user.js
 // @grant none
