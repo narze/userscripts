d5acef3c702976433399cc607ce58051edd7a6b4
Manassarn Manoonchai <manassarn@gmail.com>
2019-10-23 02:36:55 +0700

    :recycle: Format code

--- typeracer-helper.user.js
+++ typeracer-helper.user.js
@@ -2,7 +2,7 @@
 // @name TypeRacer Helper
 // @namespace https://github.com/narze/userscripts
 // @author narze
-// @version 0.1.0
+// @version 0.1.1
 // @description Shortcut keys to Typeracer, no cheats here!
 // @match https://play.typeracer.com/
 // @icon https://play.typeracer.com/favicon.ico
@@ -15,100 +15,138 @@ const LOOKAHEAD_TWOWORDS = false; // FIXME
 const ENTER = 13;
 const SPACEBAR = 32;
 
-document.onkeypress = function (e) {
+document.onkeypress = function(e) {
   let key = e.keyCode;
 
   if (key == ENTER) {
-    document.querySelector('.raceAgainLink').click();
+    document.querySelector(".raceAgainLink").click();
   }
-}
+};
 
-var body = document.querySelector('body')
-    var observer = new MutationObserver(function(mutations) {
-     if(mutations[0].addedNodes.length) {
-       if (mutations[0].addedNodes[0].className && mutations[0].addedNodes[0].className.split(' ').indexOf("countdownPopup") != -1) {
-         var target = document.querySelector("table > tbody > tr:nth-child(2) > td > table.inputPanel")
-          target.onkeydown = function(e) {
-            if (e.keyCode == SPACEBAR) {
-              setTimeout(function() {
-                var textBody = document.querySelector("table > tbody > tr:nth-child(2) > td > table.inputPanel > tbody > tr:nth-child(1) > td > table > tbody")
-                textBody.parentNode.style.position = 'relative'
+var observer = new MutationObserver(function(mutations) {
+  if (mutations[0].addedNodes.length) {
+    if (
+      mutations[0].addedNodes[0].className &&
+      mutations[0].addedNodes[0].className
+        .split(" ")
+        .indexOf("countdownPopup") != -1
+    ) {
+      var target = document.querySelector(
+        "table > tbody > tr:nth-child(2) > td > table.inputPanel"
+      );
+      target.onkeydown = function(e) {
+        if (e.keyCode == SPACEBAR) {
+          setTimeout(function() {
+            var textBody = document.querySelector(
+              "table > tbody > tr:nth-child(2) > td > table.inputPanel > tbody > tr:nth-child(1) > td > table > tbody"
+            );
+            textBody.parentNode.style.position = "relative";
 
-                var textBodyClone = textBody.cloneNode(true)
-                textBodyClone.className += "TRHelperTextMask"
+            var textBodyClone = textBody.cloneNode(true);
+            textBodyClone.className += "TRHelperTextMask";
 
-                if (!DEBUG) {
-                  textBodyClone.style.position = 'absolute'
-                  textBodyClone.style.top = '0'
-                }
-
-                var mask = "#fafafa"
-
-                if (textBodyClone.querySelectorAll('tr:nth-child(1) > td > div > div > span').length > 3) {
-                  // Typed words
-                  textBodyClone.querySelector('tr:nth-child(1) > td > div > div > span:first-child').className += ' typedWords'
-
-                  // First charactor of current word
-                  textBodyClone.querySelector('tr:nth-child(1) > td > div > div > span:nth-last-child(3)').style.backgroundColor = mask
-                  textBodyClone.querySelector('tr:nth-child(1) > td > div > div > span:nth-last-child(3)').style.color = mask
-                  textBodyClone.querySelector('tr:nth-child(1) > td > div > div > span:nth-last-child(3)').className += ' fadeOut'
-                  textBodyClone.querySelector('tr:nth-child(1) > td > div > div > span:nth-last-child(3)').style.backgroundImage = 'url("")'
-                }
-
-                // Remaining charactors of current word
-                textBodyClone.querySelector('tr:nth-child(1) > td > div > div > span:nth-last-child(2)').style.backgroundColor = mask
-                textBodyClone.querySelector('tr:nth-child(1) > td > div > div > span:nth-last-child(2)').style.color = mask
-                textBodyClone.querySelector('tr:nth-child(1) > td > div > div > span:nth-last-child(2)').className += ' fadeOut'
-                textBodyClone.querySelector('tr:nth-child(1) > td > div > div > span:nth-last-child(2)').style.backgroundImage = 'url("")'
-
-                var t = textBodyClone.querySelector('tr:nth-child(1) > td > div > div > span:last-child')
-
-                var textContent = t.textContent;
-                var wordIndex = textContent.indexOf(' ', 1);
-                var lookAheadChars = wordIndex + 1;
-
-                if (LOOKAHEAD_TWOWORDS) {
-                  lookAheadChars = textContent.indexOf(' ', lookAheadChars) + 1;
-                }
-
-                var textToHide = textContent.slice(0, lookAheadChars)
-                var textRemaining = textContent.slice(lookAheadChars)
-
-                var span = document.createElement("span");
-                var spanHide = document.createElement("span")
-                var spanRemaining = document.createElement("span");
-
-                spanHide.className += " fadeOut"
-
-                spanHide.appendChild(document.createTextNode(textToHide))
-                spanRemaining.appendChild(document.createTextNode(textRemaining))
-                spanRemaining.style.color = 'transparent'
-
-                span.appendChild(spanHide)
-                span.appendChild(spanRemaining)
-                t.parentNode.replaceChild(span, t)
-
-                textBody.parentNode.appendChild(textBodyClone)
-
-                var existingTextMasks = document.querySelectorAll("table > tbody > tr:nth-child(2) > td > table.inputPanel > tbody > tr:nth-child(1) > td > table > tbody.TRHelperTextMask")
-
-                for (let i = 0; i < existingTextMasks.length; i++) {
-                  existingTextMasks[i].style.zIndex = 100 - i
-                }
-
-                var maxTextMasks = 2
-                if (existingTextMasks && existingTextMasks.length > maxTextMasks) {
-                  for (let i = 0; i < existingTextMasks.length - maxTextMasks; i++) {
-                    textBody.parentNode.removeChild(existingTextMasks[0])
-                  }
-                }
-              }, 10)
+            if (!DEBUG) {
+              textBodyClone.style.position = "absolute";
+              textBodyClone.style.top = "0";
             }
-          }
-       }
-     }
-    });
-    // configuration of the observer:
-    var config = { childList: true };
-    // pass in the target node, as well as the observer options
-    observer.observe(body, config);
+
+            var mask = "#fafafa";
+
+            if (
+              textBodyClone.querySelectorAll(
+                "tr:nth-child(1) > td > div > div > span"
+              ).length > 3
+            ) {
+              // Typed words
+              textBodyClone.querySelector(
+                "tr:nth-child(1) > td > div > div > span:first-child"
+              ).className += " typedWords";
+
+              // First charactor of current word
+              textBodyClone.querySelector(
+                "tr:nth-child(1) > td > div > div > span:nth-last-child(3)"
+              ).style.backgroundColor = mask;
+              textBodyClone.querySelector(
+                "tr:nth-child(1) > td > div > div > span:nth-last-child(3)"
+              ).style.color = mask;
+              textBodyClone.querySelector(
+                "tr:nth-child(1) > td > div > div > span:nth-last-child(3)"
+              ).className += " fadeOut";
+              textBodyClone.querySelector(
+                "tr:nth-child(1) > td > div > div > span:nth-last-child(3)"
+              ).style.backgroundImage = 'url("")';
+            }
+
+            // Remaining charactors of current word
+            textBodyClone.querySelector(
+              "tr:nth-child(1) > td > div > div > span:nth-last-child(2)"
+            ).style.backgroundColor = mask;
+            textBodyClone.querySelector(
+              "tr:nth-child(1) > td > div > div > span:nth-last-child(2)"
+            ).style.color = mask;
+            textBodyClone.querySelector(
+              "tr:nth-child(1) > td > div > div > span:nth-last-child(2)"
+            ).className += " fadeOut";
+            textBodyClone.querySelector(
+              "tr:nth-child(1) > td > div > div > span:nth-last-child(2)"
+            ).style.backgroundImage = 'url("")';
+
+            var t = textBodyClone.querySelector(
+              "tr:nth-child(1) > td > div > div > span:last-child"
+            );
+
+            var textContent = t.textContent;
+            var wordIndex = textContent.indexOf(" ", 1);
+            var lookAheadChars = wordIndex + 1;
+
+            if (LOOKAHEAD_TWOWORDS) {
+              lookAheadChars = textContent.indexOf(" ", lookAheadChars) + 1;
+            }
+
+            var textToHide = textContent.slice(0, lookAheadChars);
+            var textRemaining = textContent.slice(lookAheadChars);
+
+            var span = document.createElement("span");
+            var spanHide = document.createElement("span");
+            var spanRemaining = document.createElement("span");
+
+            spanHide.className += " fadeOut";
+
+            spanHide.appendChild(document.createTextNode(textToHide));
+            spanRemaining.appendChild(document.createTextNode(textRemaining));
+            spanRemaining.style.color = "transparent";
+
+            span.appendChild(spanHide);
+            span.appendChild(spanRemaining);
+            t.parentNode.replaceChild(span, t);
+
+            textBody.parentNode.appendChild(textBodyClone);
+
+            var existingTextMasks = document.querySelectorAll(
+              "table > tbody > tr:nth-child(2) > td > table.inputPanel > tbody > tr:nth-child(1) > td > table > tbody.TRHelperTextMask"
+            );
+
+            for (let i = 0; i < existingTextMasks.length; i++) {
+              existingTextMasks[i].style.zIndex = 100 - i;
+            }
+
+            var maxTextMasks = 2;
+            if (existingTextMasks && existingTextMasks.length > maxTextMasks) {
+              for (
+                let i = 0;
+                i < existingTextMasks.length - maxTextMasks;
+                i++
+              ) {
+                textBody.parentNode.removeChild(existingTextMasks[0]);
+              }
+            }
+          }, 10);
+        }
+      };
+    }
+  }
+});
+
+var body = document.querySelector("body");
+var config = { childList: true };
+observer.observe(body, config);
