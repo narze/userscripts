0770e3a2e5509d801a151ec3396f0b3817cd790d
Manassarn Manoonchai <manassarn@gmail.com>
2020-04-23 17:00:54 +0700

    :sparkles: 10ff helper js - Auto redirect from front page

--- 10fastfingers-helper.user.js
+++ 10fastfingers-helper.user.js
@@ -2,8 +2,8 @@
 // @name 10FastFingers Helper
 // @namespace https://github.com/narze/userscripts
 // @author narze
-// @version 0.0.2
-// @description Quick restart
+// @version 0.1.0
+// @description Quick restart with enter & auto redirect from front page
 // @match https://10fastfingers.com/*
 // @icon https://10fastfingers.com/favicon.ico
 // @downloadURL https://github.com/narze/userscripts/raw/master/10fastfingers-helper.user.js
@@ -20,3 +20,6 @@ document.onkeypress = function (e) {
   }
 }
 
+if (document.location.href.match(/^https:\/\/10fastfingers\.com\/?$/)) {
+  document.location.href = 'https://10fastfingers.com/typing-test/english';
+}
